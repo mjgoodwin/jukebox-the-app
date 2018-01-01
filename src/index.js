8a735a4191e924ad4034bd3608006f4dfad5843f
Michael Goodwin <michael.goodwin@thescore.com>
2018-01-01 14:12:38 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// main app
+import AppContainer from './containers/AppContainer';
+
+ReactDOM.render(<AppContainer />, document.getElementById('app'))
---
+++ webpack/webpack.dev.config.js
@@ -0,0 +1,27 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var parentDir = path.join(__dirname, '../');
+
+module.exports = {
+  entry: [
+    path.join(parentDir, 'src', 'index.js')
+  ],
+  module: {
+    loaders: [{
+      test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  output: {
+    path: parentDir + '/dist',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: parentDir,
+    historyApiFallback: true,
+    port: 8080
+  }
+}
